5db376e3cdcab63d9c3c409cd0270ce39cc95d0c
Vendra <vendra0408@gmail.com>
2021-11-14 15:40:17 +0530

    Initial Commit

--- src/main/java/com/example/librarymanagementsystem/HelloApplication.java
+++ src/main/java/com/example/librarymanagementsystem/HelloApplication.java
@@ -1,5 +1,5 @@
 package com.example.librarymanagementsystem;
-
+/*
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -14,6 +14,31 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}*/
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        // Stage.initStyle(StageStyle.UNDECORATED);
+        // FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        //Scene scene = new Scene(fxmlLoader.load(), 700, 600);
+        stage.setTitle("Library Management!");
+        stage.setScene(new Scene(root, 700,600));
         stage.show();
     }
 
--- src/main/java/com/example/librarymanagementsystem/HelloController.java
+++ src/main/java/com/example/librarymanagementsystem/HelloController.java
@@ -1,7 +1,12 @@
 package com.example.librarymanagementsystem;
 
+/*import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class HelloController {
     @FXML
@@ -10,5 +15,46 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
+*/
+
+import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.Button;
+        import javafx.scene.control.Label;
+        import javafx.stage.Stage;
+        import javafx.scene.control.TextField;
+        import javafx.scene.control.PasswordField;
+
+public class HelloController {
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private Label LoginMessage;
+
+    @FXML
+    private TextField Username;
+
+    @FXML
+    private PasswordField Password;
+
+    public void loginButtonOnAction(ActionEvent e){
+        // LoginMessage.setText("Invalid Login!");
+
+        if(Username.getText().isBlank() ==  false && Password.getText().isBlank() == false) {
+            LoginMessage.setText("Invalid Login!");
+        }
+        else{
+            LoginMessage.setText("Please Enter Username And Password.");
+        }
+    }
+
+
+    public void cancelButtonOnAction(ActionEvent e){
+        Stage stage = (Stage)cancelButton.getScene().getWindow();
+        stage.close();
     }
 }
