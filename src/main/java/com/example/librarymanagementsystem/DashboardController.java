c8efbb8c649662377313ccfdabbb309d638d78f1
Vendra <vendra0408@gmail.com>
2021-11-24 12:02:05 +0530

    Initial Commit

--- src/main/java/com/example/librarymanagementsystem/DashboardController.java
+++ src/main/java/com/example/librarymanagementsystem/DashboardController.java
@@ -1,7 +1,29 @@
 package com.example.librarymanagementsystem;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class DashboardController {
-    
+
+    @FXML
+    protected void IssueBook(ActionEvent e){
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("IssueBook.fxml"));
+            ((Node)(e.getSource())).getScene().getWindow().hide();
+            Parent root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (Exception ep){
+            ep.printStackTrace();
+        }
+    }
+
 
 
 }
