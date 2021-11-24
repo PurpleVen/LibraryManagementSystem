8c9abf0f690b09c8acc4aef5f6ad58291a1b283a
Vendra <vendra0408@gmail.com>
2021-11-24 12:09:02 +0530

    Initial Commit

--- src/main/java/com/example/librarymanagementsystem/DashboardController.java
+++ src/main/java/com/example/librarymanagementsystem/DashboardController.java
@@ -24,6 +24,90 @@ public class DashboardController {
         }
     }
 
+    @FXML
+    protected void AddBook(ActionEvent e){
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AddBook.fxml"));
+            ((Node)(e.getSource())).getScene().getWindow().hide();
+            Parent root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (Exception ep){
+            ep.printStackTrace();
+        }
+    }
+
+    @FXML
+    protected void ManageDetailsBook(ActionEvent e){
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("BookDetails.fxml"));
+            ((Node)(e.getSource())).getScene().getWindow().hide();
+            Parent root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (Exception ep){
+            ep.printStackTrace();
+        }
+    }
+
+    @FXML
+    protected void AddMember(ActionEvent e){
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MemberAdd.fxml"));
+            ((Node)(e.getSource())).getScene().getWindow().hide();
+            Parent root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (Exception ep){
+            ep.printStackTrace();
+        }
+    }
+
+    @FXML
+    protected void ManageMember(ActionEvent e){
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MemberManage.fxml"));
+            ((Node)(e.getSource())).getScene().getWindow().hide();
+            Parent root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (Exception ep){
+            ep.printStackTrace();
+        }
+    }
+
+    @FXML
+    protected void DueDate(ActionEvent e){
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("DueDate.fxml"));
+            ((Node)(e.getSource())).getScene().getWindow().hide();
+            Parent root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (Exception ep){
+            ep.printStackTrace();
+        }
+    }
+
+    @FXML
+    protected void GoBackToLogin(ActionEvent e){
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
+            ((Node)(e.getSource())).getScene().getWindow().hide();
+            Parent root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (Exception ep){
+            ep.printStackTrace();
+        }
+    }
+
 
 
 }
--- src/main/java/com/example/librarymanagementsystem/IssueController.java
+++ src/main/java/com/example/librarymanagementsystem/IssueController.java
@@ -3,4 +3,5 @@ package com.example.librarymanagementsystem;
 public class IssueController {
 
 
+
 }
