f8f7a1621d9bdb663bacb6e49e1b7456d24f7b1d
Vendra <vendra0408@gmail.com>
2021-11-26 23:40:28 +0530

    Initial Commit

--- src/main/java/com/example/librarymanagementsystem/IssueController.java
+++ src/main/java/com/example/librarymanagementsystem/IssueController.java
@@ -1,6 +1,16 @@
 package com.example.librarymanagementsystem;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class IssueController {
+
+    @FXML
+    private TextField BookID;
+
+    @FXML
+    private TextField 
+
 
 
 
