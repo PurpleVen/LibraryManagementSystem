201f7d235cad6e082d58098608a620fdd25157f2
Vendra <vendra0408@gmail.com>
2021-12-01 22:29:36 +0530

    Initial Commit

--- src/main/java/com/example/librarymanagementsystem/ManageMemberController.java
+++ src/main/java/com/example/librarymanagementsystem/ManageMemberController.java
@@ -1,69 +1,14 @@
 package com.example.librarymanagementsystem;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
-import java.net.URL;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.Date;
-import java.util.ResourceBundle;
-
-public class MemberDetailsController implements Initializable{
-    /* @FXML
-private Label IssueBookLabel;*/
-    @FXML
-    private TableView<MemberDetailsinfo> table;
-    @FXML
-    private TableColumn<MemberDetailsinfo, String> name;
-    @FXML
-    private TableColumn<MemberDetailsinfo, String> memberid;
-    @FXML
-    private TableColumn<MemberDetailsinfo, String> emailid;
-    @FXML
-    private TableColumn<MemberDetailsinfo, String> phoneno;
-
-    final ObservableList<MemberDetailsinfo> listview1 = FXCollections.observableArrayList();
-    @Override
-    public void initialize(URL url, ResourceBundle rb){
-        name.setCellValueFactory(new PropertyValueFactory<>("Name"));
-        memberid.setCellValueFactory(new PropertyValueFactory<>("MemberID"));
-        emailid.setCellValueFactory(new PropertyValueFactory<>("EmailID"));
-        phoneno.setCellValueFactory(new PropertyValueFactory<>("PhoneNo"));
-        try{
-            DatabaseConnector connectnow = new DatabaseConnector();
-            Connection connectdb = connectnow.getConnection();
-
-            String sql = "select *  from membermanage";
-            Statement s = connectdb.createStatement();
-            ResultSet resultSet = s.executeQuery(sql);
-
-            while (resultSet.next()){
-                listview1.add(new MemberDetailsinfo(resultSet.getString("Name"),
-                        resultSet.getString("MemberID"),
-                        resultSet.getString("EmailID"),
-                        resultSet.getString("PhoneNo")));
-            }
-            table.setItems(listview1);
-
-        }
-        catch (Exception ep){
-            ep.printStackTrace();
-
-        }
-    }
+public class ManageMemberController {
     @FXML
     protected void GoToDashboard(ActionEvent e)
 
@@ -186,6 +131,7 @@ private Label IssueBookLabel;*/
             ep.printStackTrace();
         }
     }
+
 
 
 }
