d283007a56cbe483c55eef3c890517b1eaec21ee
Om_jannu <omjannu2002@gmail.com>
2021-12-06 22:00:26 +0530

    almost done

--- src/main/java/com/example/librarymanagementsystem/ManageMemberController.java
+++ src/main/java/com/example/librarymanagementsystem/ManageMemberController.java
@@ -6,9 +6,72 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 public class ManageMemberController {
+    @FXML
+    private Button EditMemberButton;
+    @FXML
+    private Button DeleteMemberButton;
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField member_id;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField phone_no;
+    @FXML
+    private Label StatusLabel;
+
+
+
+    public void button(ActionEvent event){
+        if(event.getSource() == EditMemberButton){
+            EditMemberDetails();
+        }
+        else if (event.getSource() == DeleteMemberButton){
+            DeleteMemberDetails();
+
+        }
+
+    }
+
+    private void EditMemberDetails(){
+        DatabaseConnector connectnow = new DatabaseConnector();
+        Connection connectdb = connectnow.getConnection();
+        String query = "Update membermanage set Name = '"+name.getText()+"',MemberID = "+member_id.getText()+",EmailID= '"+email.getText()+"',PhoneNo='"+phone_no.getText()+"' where MemberID="+member_id.getText()+ "";
+        executeQuery(query);
+        StatusLabel.setText("Member Updated Successfully");
+    }
+
+    private void executeQuery(String query){
+        DatabaseConnector connectnow = new DatabaseConnector();
+        Connection connectdb = connectnow.getConnection();
+        Statement statement;
+        try{
+            statement = connectdb.createStatement();
+            statement.executeUpdate(query);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+    }
+
+    private void DeleteMemberDetails(){
+        DatabaseConnector connectnow = new DatabaseConnector();
+        Connection connectdb = connectnow.getConnection();
+        String query = "delete from membermanage where  MemberID="+member_id.getText()+"";
+        executeQuery(query);
+        StatusLabel.setText("Member Deleted Successfully");
+
+    }
     @FXML
     protected void GoToDashboard(ActionEvent e)
 
